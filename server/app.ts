34907878dfdff3fe7b521451ed7623b6b3cdc0f8
Limarenko Denis <limarenkodenis@gmail.com>
2017-05-02 08:46:40 +0300

    test

--- server/app.ts
+++ server/app.ts
@@ -1,149 +1,50 @@
-// const {Server} = require('http');
+// import http = require('http');
+// import * as url from 'url';
+// // const count = require('./count');
+// import count = require('./count'); // ok
 
-// let i = 0;
-
-// const server = new Server((req, res) => {
-//   debugger;
-//   i += 1;
-//   res.end(i.toString()); // (!!! toString) Buffer
+// const server = http.createServer((req, res) => {
+//   const urlParse = url.parse(req.url, true, true);
+//   console.log(count());
+//   res.end();
 // });
-
 // server.listen(8000);
-
-// const {Server} = require('http');
-// import {Server} from 'http';
-// import {createServer} from 'http';
-
-// let i: number = 0;
-// const server = new Server((req, res) => {
-//   i++;
-//   res.end(i.toString()); // (!!! toString)
-// });
-
-// server.listen(8000);
-
-
-// import {createServer} from 'http';
-// import {count} from './count';
-
-// let i: number = 0;
-// const server = createServer((req, res) => {
-//   count();
-//   i++;
-//   res.end(i.toString());
-// });
-// server.listen(3000);
-
-import * as http from 'http';
-// const http = require('http');
-
-const server = http.Server((req, res) => {
-  count();
-  i++;
-  res.end(i.toString());
-});
-server.listen(3000);
-
-
-
-
-
-
-const {Server} = require('http');
-const server = new Server((req, res) => {}); // ok
+// ok
+// =============================
 //
-import {Server} from 'http';
-const server = new Server((req, res) => {}); // - Cannot find name 'Server'
-
+// const {Server} = require('http');
+// const server = new Server((req, res) => {}); // ok
+//
+// import {Server} from 'http';
+// const server = new Server((req, res) => {}); // - Cannot find name 'Server'
+//
+//
+// =============================
+//
 // и тут также
-const http = require('http');
-const server = http.Server((req, res) => {.. // ok
-
-import * as http from 'http';
-const server = http.Server((req, res) => {
+// const http = require('http');
+// const server = http.Server((req, res) => {}) // ok
+//
+// import * as http from 'http';
+// const server = http.createServer((req, res) => { // ok
+// const server = http.Server((req, res) => { // err
 // Property 'Server' does not exist on type 'typeof "http"'.
-
-
-
-
-
-
-
-
-
-
-// 'use strict';
-
-// let url = require('url');
-// let fs = require('fs');
-
-// require('http').createServer(function(req, res) {
-
-//   const pathname: string = decodeURI(url.parse(req.url).pathname);
-
-//   switch (req.method) {
-//   case 'GET':
-//     if (pathname === '/') {
-//       // отдачу файлов следует переделать "правильно", через потоки, с нормальной обработкой ошибок
-//       fs.readFile(__dirname + '/public/index.html', (err, content) => {
-//         if (err) throw err;
-//         res.setHeader('Content-Type', 'text/html;charset=utf-8');
-//         res.end(content);
-//       });
-//       return;
-//     }
-
-//   default:
-//     res.statusCode = 502;
-//     res.end("Not implemented");
+//
+// =============================
+//
+// const { Server } = require('http');
+//
+// const server = new Server();
+// server.on('request', (req, res) => {
+//   if (req.url === '/') {
+//     res.end('Hello, world!');
 //   }
+// });
+//
+// server.listen(8000);
+//
+//
 
-// }).listen(3000);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// 'use strict';
-
-// let url = require('url');
-// let fs = require('fs');
-
-// require('http').createServer(function(req, res) {
-
-//   const pathname: string = decodeURI(url.parse(req.url).pathname);
-
-//   switch (req.method) {
-//   case 'GET':
-//     if (pathname === '/') {
-//       // отдачу файлов следует переделать "правильно", через потоки, с нормальной обработкой ошибок
-//       fs.readFile(__dirname + '/public/index.html', (err, content) => {
-//         if (err) throw err;
-//         res.setHeader('Content-Type', 'text/html;charset=utf-8');
-//         res.end(content);
-//       });
-//       return;
-//     }
-
-//   default:
-//     res.statusCode = 502;
-//     res.end("Not implemented");
-//   }
-
-// }).listen(3000);
+// import { Server } from 'http';
+// const server = new Server();
 
--- server/count.ts
+++ server/count.ts
@@ -1,3 +1,11 @@
-export let count = (): void => {
-  console.log(1);
+// let i: number = 0;
+// module.exports = () => {
+//   i++;
+//   console.log(i);
+// };
+
+let i: number = 0;
+export = () => {
+  i++;
+  console.log(i);
 };
